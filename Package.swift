d0794f0e075442af0d13ff9d12584bdc08aed11d
Stoyan Stoyanov <stoqn.stoqnov.93@gmail.com>
2019-06-01 13:12:02 +0100

    removed storage package

--- Package.swift
+++ Package.swift
@@ -12,12 +12,9 @@ let package = Package(
 
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
-        
-        // A package to ease the use of multiple storage and CDN services.
-        .package(url: "https://github.com/nodes-vapor/storage.git", from: "1.0.0-beta")
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "Storage"]),
+        .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
